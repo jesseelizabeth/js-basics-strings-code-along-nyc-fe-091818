e00fb8c2e3a7edac705532447f9a5ac1cd301623
Jesse Evans <jesseelizabeth1@gmail.com>
2018-10-17 00:46:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ let currentUser = 'Jesse'
 let welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!'
 
 let excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser.toUpperCase() + '!'
+
+let shortGreeting = 'Welcome, ' + currentUser + '!'
